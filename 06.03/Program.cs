43fc41c2585b2c9a910bb5fd2963b9ede8805117
pgajgfj <121456433+pgajgfj@users.noreply.github.com>
2024-07-05 16:02:57 +0300

    Add files via upload

---
+++ 06.03/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["StorageDB"].ConnectionString;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                
+                CreateTables(connection);
+
+                
+                InsertData(connection);
+
+
+                UpdateData(connection);
+
+
+                DeleteData(connection);
+            }
+        }
+
+        static void CreateTables(SqlConnection connection)
+        {
+            using (var command = new SqlCommand("IF OBJECT_ID('Products', 'U') IS NULL CREATE TABLE Products (Id INT PRIMARY KEY, Name NVARCHAR(50), TypeId INT, SupplierId INT, Quantity INT, Cost DECIMAL(10, 2), DeliveryDate DATETIME)", connection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("IF OBJECT_ID('Types', 'U') IS NULL CREATE TABLE Types (Id INT PRIMARY KEY, Name NVARCHAR(50))", connection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("IF OBJECT_ID('Suppliers', 'U') IS NULL CREATE TABLE Suppliers (Id INT PRIMARY KEY, Name NVARCHAR(50))", connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        static void InsertData(SqlConnection connection)
+        {
+            using (var command = new SqlCommand("INSERT INTO Types (Name) VALUES (@Name)", connection))
+            {
+                command.Parameters.AddWithValue("@Name", "Тип 1");
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("INSERT INTO Suppliers (Name) VALUES (@Name)", connection))
+            {
+                command.Parameters.AddWithValue("@Name", "Постачальник 1");
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("INSERT INTO Products (Name, TypeId, SupplierId, Quantity, Cost, DeliveryDate) VALUES (@Name, @TypeId, @SupplierId, @Quantity, @Cost, @DeliveryDate)", connection))
+            {
+                command.Parameters.AddWithValue("@Name", "Товар 1");
+                command.Parameters.AddWithValue("@TypeId", 1);
+                command.Parameters.AddWithValue("@SupplierId", 1);
+                command.Parameters.AddWithValue("@Quantity", 10);
+                command.Parameters.AddWithValue("@Cost", 10.5m);
+                command.Parameters.AddWithValue("@DeliveryDate", DateTime.Now);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        static void UpdateData(SqlConnection connection)
+        {
+            using (var command = new SqlCommand("UPDATE Products SET Name = @Name, Quantity = @Quantity WHERE Id = @Id", connection))
+            {
+                command.Parameters.AddWithValue("@Id", 1);
+                command.Parameters.AddWithValue("@Name", "Товар 1 (оновлено)");
+                command.Parameters.AddWithValue("@Quantity", 15);
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("UPDATE Types SET Name = @Name WHERE Id = @Id", connection))
+            {
+                command.Parameters.AddWithValue("@Id", 1);
+                command.Parameters.AddWithValue("@Name", "Тип 1 (оновлено)");
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("UPDATE Suppliers SET Name = @Name WHERE Id = @Id", connection))
+            {
+                command.Parameters.AddWithValue("@Id", 1);
+                command.Parameters.AddWithValue("@Name", "Постачальник 1 (оновлено)");
+                command.ExecuteNonQuery();
+            }
+        }
+
+        static void DeleteData(SqlConnection connection)
+        {
+            using (var command = new SqlCommand("DELETE FROM Products WHERE Id = @Id", connection))
+            {
+                command.Parameters.AddWithValue("@Id", 1);
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("DELETE FROM Types WHERE Id = @Id", connection))
+            {
+                command.Parameters.AddWithValue("@Id", 1);
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = new SqlCommand("DELETE FROM Suppliers WHERE Id = @Id", connection))
+            {
+                command.Parameters.AddWithValue("@Id", 1);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ 15.3/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace MusicApp
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Artist> Artists { get; set; }
+    }
+
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+
+        public int Year { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Genre { get; set; }
+
+        public byte[] CoverImage { get; set; }
+
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+
+    public class Track
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public virtual ICollection<PlaylistTrack> PlaylistTracks { get; set; }
+    }
+
+    public class Playlist
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual ICollection<PlaylistTrack> PlaylistTracks { get; set; }
+    }
+
+    public class PlaylistTrack
+    {
+        public int Id { get; set; }
+
+        public int PlaylistId { get; set; }
+        public Playlist Playlist { get; set; }
+
+        public int TrackId { get; set; }
+        public Track Track { get; set; }
+    }
+
+    public class MusicAppContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<PlaylistTrack> PlaylistTracks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>()
+                .HasRequired(a => a.Country)
+                .WithMany(c => c.Artists)
+                .HasForeignKey(a => a.CountryId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Album>()
+                .HasRequired(a => a.Artist)
+                .WithMany(a => a.Albums)
+                .HasForeignKey(a => a.ArtistId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Track>()
+                .HasRequired(t => t.Album)
+                .WithMany(a => a.Tracks)
+                .HasForeignKey(t => t.AlbumId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<PlaylistTrack>()
+                .HasKey(pt => new { pt.PlaylistId, pt.TrackId });
+
+            modelBuilder.Entity<PlaylistTrack>()
+                .HasRequired(pt => pt.Playlist)
+                .WithMany(p => p.PlaylistTracks)
+                .HasForeignKey(pt => pt.PlaylistId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<PlaylistTrack>()
+                .HasRequired(pt => pt.Track)
+                .WithMany(t => t.PlaylistTracks)
+                .HasForeignKey(pt => pt.TrackId)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
