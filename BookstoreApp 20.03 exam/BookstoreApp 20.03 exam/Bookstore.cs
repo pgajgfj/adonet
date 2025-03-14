01823d15468a07d83805ff8423fd93ba744ffcd2
pgajgfj <121456433+pgajgfj@users.noreply.github.com>
2025-03-14 23:48:24 +0200

    Add files via upload

---
+++ BookstoreApp 20.03 exam/BookstoreApp 20.03 exam/Bookstore.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class Bookstore
+{
+    private readonly string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Bookstore;Trusted_Connection=True;";
+
+    public bool RegisterUser(string username, string password)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Users (Username, Password) VALUES (@Username, @Password)";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                return command.ExecuteNonQuery() > 0;
+            }
+        }
+    }
+
+    public bool UserExists(string username, string password)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                return (int)command.ExecuteScalar() > 0;
+            }
+        }
+    }
+
+    public void AddBook(string title, int authorId, int publisherId, int genreId, int pages, int year, decimal cost, decimal salePrice, bool isSequel, int sequelBookId)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Books (Title, AuthorId, PublisherId, GenreId, Pages, Year, Cost, SalePrice, IsSequel, SequelBookId) " +
+                           "VALUES (@Title, @AuthorId, @PublisherId, @GenreId, @Pages, @Year, @Cost, @SalePrice, @IsSequel, @SequelBookId)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Title", title);
+                command.Parameters.AddWithValue("@AuthorId", authorId);
+                command.Parameters.AddWithValue("@PublisherId", publisherId);
+                command.Parameters.AddWithValue("@GenreId", genreId);
+                command.Parameters.AddWithValue("@Pages", pages);
+                command.Parameters.AddWithValue("@Year", year);
+                command.Parameters.AddWithValue("@Cost", cost);
+                command.Parameters.AddWithValue("@SalePrice", salePrice);
+                command.Parameters.AddWithValue("@IsSequel", isSequel);
+                command.Parameters.AddWithValue("@SequelBookId", sequelBookId == 0 ? DBNull.Value : (object)sequelBookId);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void ViewAllBooks()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT Title, Pages, Year, SalePrice FROM Books";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                Console.WriteLine("\n--- Список усіх книг ---");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Назва: {reader["Title"]}, Сторінок: {reader["Pages"]}, Рік: {reader["Year"]}, Ціна: {reader["SalePrice"]} грн");
+                }
+            }
+        }
+
+        Console.WriteLine("\nНатисніть будь-яку клавішу для повернення в меню...");
+        Console.ReadKey();
+    }
+
+    public void SearchBooks(string title = null, string author = null, string genre = null)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT * FROM Books WHERE " +
+                           "(Title LIKE @Title OR @Title IS NULL) AND " +
+                           "(AuthorId IN (SELECT Id FROM Authors WHERE Name LIKE @Author) OR @Author IS NULL) AND " +
+                           "(GenreId IN (SELECT Id FROM Genres WHERE Name LIKE @Genre) OR @Genre IS NULL)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Title", title != null ? $"%{title}%" : (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Author", author != null ? $"%{author}%" : (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Genre", genre != null ? $"%{genre}%" : (object)DBNull.Value);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Назва: {reader["Title"]}, Рік: {reader["Year"]}, Ціна: {reader["SalePrice"]} грн");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BookstoreApp 20.03 exam/BookstoreApp 20.03 exam/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var bookstore = new Bookstore();
+
+        bool isRunning = true;
+        while (isRunning)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Реєстрація");
+            Console.WriteLine("2. Вхід");
+            Console.WriteLine("3. Додати книгу");
+            Console.WriteLine("4. Переглянути всі книги");
+            Console.WriteLine("5. Вийти");
+            Console.Write("Виберіть опцію: ");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    RegisterUser(bookstore);
+                    break;
+                case "2":
+                    LoginUser(bookstore);
+                    break;
+                case "3":
+                    AddBook(bookstore);
+                    break;
+                case "4":
+                    bookstore.ViewAllBooks();
+                    break;
+                case "5":
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Невірна опція. Спробуйте ще раз.");
+                    break;
+            }
+        }
+
+        Console.WriteLine("Програма завершена.");
+    }
+
+    private static void RegisterUser(Bookstore bookstore)
+    {
+        Console.Write("Введіть логін: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Введіть пароль: ");
+        string password = Console.ReadLine();
+
+        if (bookstore.RegisterUser(username, password))
+            Console.WriteLine("Реєстрація успішна!");
+        else
+            Console.WriteLine("Користувач з таким логіном вже існує.");
+
+        Console.ReadKey();
+    }
+
+    private static void LoginUser(Bookstore bookstore)
+    {
+        Console.Write("Введіть логін: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Введіть пароль: ");
+        string password = Console.ReadLine();
+
+        if (bookstore.UserExists(username, password))
+            Console.WriteLine("Вхід успішний!");
+        else
+            Console.WriteLine("Невірний логін або пароль.");
+
+        Console.ReadKey();
+    }
+
+    private static void AddBook(Bookstore bookstore)
+    {
+        Console.Write("Назва книги: ");
+        string title = Console.ReadLine();
+
+        int authorId = ReadIntInput("ID автора");
+        int publisherId = ReadIntInput("ID видавництва");
+        int genreId = ReadIntInput("ID жанру");
+        int pages = ReadIntInput("Кількість сторінок");
+        int year = ReadIntInput("Рік видання");
+
+        Console.Write("Собівартість книги: ");
+        decimal cost = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Ціна продажу: ");
+        decimal salePrice = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Чи є книга продовженням іншої? (true/false): ");
+        bool isSequel = bool.Parse(Console.ReadLine());
+
+        int sequelBookId = 0;
+        if (isSequel)
+        {
+            sequelBookId = ReadIntInput("ID попередньої книги");
+        }
+
+        bookstore.AddBook(title, authorId, publisherId, genreId, pages, year, cost, salePrice, isSequel, sequelBookId);
+        Console.WriteLine("Книга успішно додана!");
+
+        Console.ReadKey();
+    }
+
+    private static int ReadIntInput(string prompt)
+    {
+        int value;
+        while (true)
+        {
+            Console.Write($"{prompt}: ");
+            if (int.TryParse(Console.ReadLine(), out value))
+                return value;
+
+            Console.WriteLine("Некоректне значення. Введіть число.");
+        }
+    }
+}
